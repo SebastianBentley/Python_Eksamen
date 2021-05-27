bf22020cbac2eef754e660f699b7f1c10e8a094c
Sebastian <cph-sb287@cphbusiness.dk>
2021-05-27 10:13:42 +0200

    top 5

---
+++ close_five.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+def get_close_five(name, price):
+    #filepaths
+    filepath = 'laptop_csv_data.csv'
+    filepath2 = 'clusters.csv'
+    #load the csv files
+    laptops = pd.read_csv(filepath)
+    clusters = pd.read_csv(filepath2)
+
+    #find the specific cluster to the laptop
+    cluster_price = min(list(clusters['price']), key=lambda x:abs(x-price))
+    found_cluster = clusters.loc[clusters['price'] == cluster_price]['cluster_group']
+
+    #Find the laptops in the same cluster
+    same_cluster = laptops.loc[laptops['cluster_group'] == float(found_cluster)][['name','price']]
+    #Find different laptops, than the one being searched
+    final_laptops = same_cluster.loc[same_cluster['name'] != name][['name', 'price']]
+    
+    #Get the final close-priced laptops
+    results = final_laptops.iloc[(same_cluster['price']-price).abs().argsort()[:5]]
+    return results
+
+if __name__ == "__main__":
+    print(get_close_five("lenovo blade 13", 14560))
+
+
+
+
+
+
--- genclusters.py
+++ genclusters.py
@@ -5,14 +5,24 @@ from sklearn.cluster import estimate_bandwidth
 from sklearn.cluster import MeanShift
 
 def create_clusters():
+    #Read the csv data
     laptop_data = pd.read_csv('laptops.csv')
+    #Apply columns
     laptop_data.columns = ["name", "price"]
+    #use label encoder, and change names to numeric values
     label_enc =preprocessing.LabelEncoder()
     laptop_data['name'] = label_enc.fit_transform(laptop_data['name'].astype(str))
+    #Estimate the bandwith
     est_bandwidth = estimate_bandwidth(laptop_data)
-    analyzer = MeanShift(bandwidth=3000) 
+    #Choose a different bandwith, because it generates more clusters (a lot of laptops are within the same pricerange)
+    analyzer = MeanShift(bandwidth=3000)
+    #fit the data
     analyzer.fit(laptop_data)
     labels = analyzer.labels_
+    #Change the names back to alphabetical value
+    laptop_data['name'] = label_enc.inverse_transform(laptop_data['name'])
+
+    #insert the cluster group to each laptop
     laptop_data['cluster_group'] = np.nan
     for i in range(len(laptop_data)):
         laptop_data.iloc[i,laptop_data.columns.get_loc('cluster_group')] = labels[i] #set the cluster label on each row
@@ -20,8 +30,12 @@ def create_clusters():
     laptop_cluster_data = laptop_data.groupby(['cluster_group']).mean()
     #Count of laptops in each cluster
     laptop_cluster_data['Counts'] = pd.Series(laptop_data.groupby(['cluster_group']).size())
-    return laptop_cluster_data
+
+    #Save the new dataset with clusters to a file
+    laptop_data.to_csv("laptop_csv_data.csv")
+    #Save the cluster groups to a file
+    laptop_cluster_data.to_csv("clusters.csv")
 
 
 if __name__ == "__main__":
-    create_clusters().to_csv("clusters.csv")
+    create_clusters()
